2abb539cf58d02b8c554a06b2ec74dc4ce1a94b7
Esteban Isea <iseaesteban@gmail.com>
2021-01-17 18:17:05 -0400

    sorting projects by descendent order of buiding

--- src/client.js
+++ src/client.js
@@ -1,13 +1,12 @@
-import {GraphQLClient} from "graphql-request";
+import { GraphQLClient } from "graphql-request"
 
-const apiToken = process.env.REACT_APP_DATO_API_TOKEN;
-const endpoint = `https://graphql.datocms.com`;
+const apiToken = "f0d2ed0861a84cd6fc403e2a573a84"
+const endpoint = `https://graphql.datocms.com`
 const headers = {
-	Accept: "application/json",
-	"Content-Type": "application/json",
-	Authorization: "Bearer " + apiToken
-};
+  Accept: "application/json",
+  "Content-Type": "application/json",
+  Authorization: "Bearer " + apiToken,
+}
 
-
-const client = new GraphQLClient(endpoint, {headers});
-export default client;
+const client = new GraphQLClient(endpoint, { headers })
+export default client
